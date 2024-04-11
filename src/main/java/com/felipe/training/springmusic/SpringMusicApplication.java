96eaa48f620ee57b95fc9f68f01e4cfcd955adbf
Felipe Soares <65181945+felipe010101@users.noreply.github.com>
2024-04-11 11:09:36 -0300

    first commit

---
+++ src/main/java/com/felipe/training/springmusic/SpringMusicApplication.java
@@ -0,0 +1,13 @@
+package com.felipe.training.springmusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMusicApplication.class, args);
+	}
+
+}
